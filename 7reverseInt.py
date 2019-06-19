7763130e10bc6f26de43d273154f1d5e6fd83e5f
wangfan <wangfanicq@163.com>
2019-06-19 17:44:53 +0800

    update file7

--- 7reverseInt.py
+++ 7reverseInt.py
@@ -13,3 +13,17 @@ class Solution:
 a=Solution()
 b=int(input())
 print(a.reverseInt(b))
+'''
+#following way2 not using string
+class Solution:
+    def reverseInt(self,x):
+        if not x:
+            return True
+        if x<0 or x%10==0:
+            return False
+        tmp=0
+        while x>tmp:
+            tmp=tmp*10+x%10
+            x//=10
+        return x==tmp or x==tmp//10
+'''
